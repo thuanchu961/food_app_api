9051c47a357d8767bc6d181c35369eb32afea2ac
dovanminhdung2001@gmail.com <140503031711As>
2024-01-04 16:25:46 +0700

    AES demo

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,18 @@
+import e from 'express';
 import pool from '../database/index.js'
+import crypto from 'crypto'; 
+
+const salt = 'thuantoandungnhom3monbaomat!@#$%';
+const AESKey = 'ngay10/03/2001langaygiotohangnam' ;  //256-bit 32 ký tự 
+const AESIv = '1234567890121416' ; // 12 ký tự
 //đã sửa
 export const signup = async (req, res) => {
-  const { username, password, email, fullname, gender, dateofbirth, phone } = req.body
+  let { username, password, email, fullname, gender, dateofbirth, phone } = req.body;
+  password = hashPassword(password);
+  
+  if(!Object.is(phone, null)) 
+    phone = AESData(phone);
+   
   try {
     const checkExisted = await pool.query(
       `SELECT * FROM users WHERE email = '${email}'`,
@@ -21,7 +32,15 @@ export const signup = async (req, res) => {
 }
 //đã sửa
 export const signin = async (req, res) => {
-  const { email, password } = req.body
+  // let data = '123456' ;  
+  // let encr = AESData(data); 
+  // console.log('data: ', data);
+  // console.log('encr: ', encr);
+  // let decr  = DeAESData(encr);
+  // console.log('decr: ', decr);
+
+  let { email, password } = req.body;
+  password = hashPassword(password);
   try {
     const checkExisted = await pool.query(
       `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`,
@@ -60,7 +79,10 @@ export const getAllUsers = async (req, res) => {
 //đã sửa
 export const updateUser = async (req, res) => {
   const { userid } = req.params
-  const { fullname, gender, dateofbirth, phone, address } = req.body
+  let { fullname, gender, dateofbirth, phone, address } = req.body
+
+  if(!Object.is(address, null))
+    address = AESData(address);
 
   try {
     const result = await pool.query(
@@ -83,7 +105,10 @@ export const updateUser = async (req, res) => {
 //đã sửa
 export const updateAddress = async (req, res) => {
   const { userid } = req.params
-  const { address } = req.body
+  let { address } = req.body
+
+  if(!Object.is(address, null))
+    address = AESData(address);
 
   try {
     const result = await pool.query(
@@ -98,4 +123,24 @@ export const updateAddress = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error })
   }
+} 
+// Hàm để hash mật khẩu
+function hashPassword(password) { 
+  // crypto.randomBytes(16).toString('hex');
+  const hashedPassword = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return hashedPassword;
+}
+// Hàm AES   sdt và  adress
+function AESData(data) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', AESKey, AESIv);
+  let encrypted  = cipher.update(data, 'utf-8', 'hex'); 
+  encrypted += cipher.final('hex'); 
+  return encrypted;
+}
+// demo  decrypt
+function DeAESData(encr) {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', AESKey, AESIv);
+  let decrypted   = decipher.update(encr, 'hex', 'utf-8'); 
+  decrypted  += decipher.final('utf-8'); 
+  return decrypted ;
 }
