18493176eb15248c23ad82598358a6ba31f3e0bc
thuanchu961 <82590983+thuanchu961@users.noreply.github.com>
2024-01-07 18:17:07 +0700

    modife auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,33 +16,19 @@ export const isAuth = async (req, res, next) => { //check token
       return res.status(498).send('You do not have access to this feature!')
     }
 
-    const admin = await pool.query(
-      `SELECT * FROM admins WHERE admin_id = '${verified.payload.admin_id}'`,
-    );
-
-    const user = await pool.query(
-      `SELECT * FROM users WHERE user_id = '${verified.payload.user_id}'`,
-    );
-
-    // Kiểm tra và gán quyền truy cập vào req.user
-    if (admin.rows.length > 0) {
+    if(verified.payload.role === "admin"){
+      const admin = await pool.query(
+        `SELECT * FROM admins WHERE admin_id = '${verified.payload.admin_id}'`,
+      );
       req.user = admin;
-    } else if (user.rows.length > 0) {
+      next();
+    }else{
+      const user = await pool.query(
+        `SELECT * FROM users WHERE user_id = '${verified.payload.user_id}'`,
+      );
       req.user = user;
-    } else {
-      return res.status(401).send('User not found!');
-    }
-
-    if (req.user.role == "admin") {
-      // Thực hiện các chức năng dành cho admin
-      // console.log('Admin access');
-      next();
-    } else {
-      // Thực hiện các chức năng dành cho user
-      // console.log('User access');
       next();
     }
-
     // req.user = user
      //gọi hàm tiếp theo trong router
   } catch (error) {
